b34a77f45de7445b21a0848c537f5654590ed85b
Elton <eltonmcrvlh@gmail.com>
2024-01-14 10:56:36 -0300

    feat[qrcode]: add link

---
+++ src/pages/QrCode/index.tsx
@@ -0,0 +1,8 @@
+
+const QrCode = () => {
+  return (
+    window.location.replace('https://wa.me/5579996423641')
+  );
+};
+
+export default QrCode;
--- src/router/config.ts
+++ src/router/config.ts
@@ -4,6 +4,12 @@ const routes = [
     exact: true,
     component: "Home",
   },
+  {
+    path: ["/qrcode"],
+    exact: true,
+    component: "QrCode",
+  },
+  
 ];
 
 export default routes;
